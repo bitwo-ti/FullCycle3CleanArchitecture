2cf48ea6048deb5028edd307b927616623e83034
Marco Antonio <marco.veludo@sankhya.com.br>
2024-04-04 09:24:24 -0300

    fix: ajuste para entregar o Desafio: Notification Pattern em Products

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,6 +1,5 @@
 import Entity from "../../@shared/entity/entity.abstract";
 import NotificationError from "../../@shared/notification/notification.error";
-import ProductValidatorFactory from "../factory/product.validator.factory";
 import ProductInterface from "./product.interface";
 
 export default class Product extends Entity implements ProductInterface {
@@ -40,7 +39,16 @@ export default class Product extends Entity implements ProductInterface {
     this.validate();
   }
 
-  validate() {
-    ProductValidatorFactory.create().validate(this);
+  validate(): boolean {
+    if (this._id.length === 0) {
+      this.notification.addError({ context: "product", message: "Id is required" });
+    }
+    if (this._name.length === 0) {
+      this.notification.addError({ context: "product", message: "Name is required" });
+    }
+    if (this._price < 0) {
+      this.notification.addError({ context: "product", message: "Price must be greater than zero" });
+    }
+    return true;
   }
 }
